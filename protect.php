add38731d226d06de4381c2b38714c995c5543ec
Gabriel S. Jesus <gabriel_live12@outlook.com>
2024-08-16 00:16:29 -0300

    criado para proteger a url

---
+++ protect.php
@@ -0,0 +1,11 @@
+<?php
+
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+if(!isset($_SESSION['id'])) {
+    die("Você não pode acessar essa página.<p><a href=\"index.php\">Entrar<a/> </p>");
+}
+
+?>
